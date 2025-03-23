9e22dfb6905bc4bf9aba93c5b80c97573c8d47ba
Price Hiller <price@price-hiller.com>
2025-03-23 14:19:43 -0500

    refactor: include .clang-format, extract C code to `src` directory

--- src/CompArch_CacheCalculation.c
+++ src/CompArch_CacheCalculation.c
@@ -1,33 +1,39 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>
 
 // cache calculation
 
-int calculateBlocks(int cacheSize, int blockSize) { return cacheSize/blockSize; }
-int calculateSets(int cacheSize, int blockSize, int associativity){ return cacheSize/(blockSize * associativity); }
+int calculateBlocks(int cacheSize, int blockSize) {
+    return cacheSize / blockSize;
+}
+int calculateSets(int cacheSize, int blockSize, int associativity) {
+    return cacheSize / (blockSize * associativity);
+}
 int calculateOffsetSize(int associativity) { return log2f(associativity); }
 int calculateIndexSize(int sets) { return log2f(sets); }
-int calculateTagSize(int cacheBits, int indexSize, int offsetSize) { return cacheBits - indexSize - offsetSize; }
-
-int calculateOverheadSize(int numBlocks, int tagSize){
-    int overhead = numBlocks*(1+tagSize);
-    return overhead/8;
+int calculateTagSize(int cacheBits, int indexSize, int offsetSize) {
+    return cacheBits - indexSize - offsetSize;
 }
 
-double calculateImplementationMemorySize(int cacheSize, int overheadSize){
+int calculateOverheadSize(int numBlocks, int tagSize) {
+    int overhead = numBlocks * (1 + tagSize);
+    return overhead / 8;
+}
+
+double calculateImplementationMemorySize(int cacheSize, int overheadSize) {
     return cacheSize + overheadSize;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
     // printf("%.2lf", log2f(512));
-    if (argc < 2){
+    if (argc < 2) {
         printf("Not enough args.");
         return 1;
     }
-    
-    int cacheSize = 512 * pow(2,10);
+
+    int cacheSize = 512 * pow(2, 10);
     int cacheBits = log2f(cacheSize);
 
     int blockSize = atoi(argv[1]);
@@ -38,8 +44,9 @@ int main(int argc, char* argv[]) {
     int offsetSize = calculateOffsetSize(associativity);
     int tagSize = calculateTagSize(cacheBits, indexSize, offsetSize);
     int overheadSize = calculateOverheadSize(numBlocks, tagSize);
-    int implementMemSize = calculateImplementationMemorySize(cacheSize, overheadSize);
-    double implementMemSizeKB = (double)(implementMemSize/pow(2,10));
+    int implementMemSize =
+        calculateImplementationMemorySize(cacheSize, overheadSize);
+    double implementMemSizeKB = (double)(implementMemSize / pow(2, 10));
     printf("Total # Blocks: \t\t%d\n", calculateBlocks(cacheSize, blockSize));
     printf("Cache Bits: \t\t\t%d\n", cacheBits);
     printf("Tag Size: \t\t\t%d bits\n", tagSize);
@@ -47,7 +54,8 @@ int main(int argc, char* argv[]) {
     printf("Offset Size: \t\t\t%d bits\n", offsetSize);
     printf("Total # Rows: \t\t\t%d\n", sets);
     printf("Overhead Size: \t\t\t%d bytes\n", overheadSize);
-    printf("Implementation Memory Size: \t%.2lf KB (%d bytes)\n", implementMemSizeKB, implementMemSize);
+    printf("Implementation Memory Size: \t%.2lf KB (%d bytes)\n",
+           implementMemSizeKB, implementMemSize);
 
     return 0;
 }
