391456a9fc4ea1885d0d7a7c74fd7b4debac26f2
perraas <80073008+perraas@users.noreply.github.com>
2025-04-02 15:44:06 -0500

    Update physmemcalc.c

--- src/physmemcalc.c
+++ src/physmemcalc.c
@@ -1,43 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
-#define PAGE_SIZE 4096  // bytes / page
+#define PAGE_SIZE 4096  // bytes per page
 #define VIRTUAL_ADDRESS_SPACE_BYTES (2ULL * 1024 * 1024 * 1024)
 
-void calculateMemoryStats(int physmem, int os, int files) {
-    // physical memory to bytes
-    int physBytes = physmem * 1024 * 1024;
-    // total # of physical pages
-    int numPages = physBytes / PAGE_SIZE;
+typedef struct {
+    int physBytes;                // Physical memory in bytes
+    int numPages;                 //  # of physical pages
+    int numOSPages;               // # of OS pages
+    int validBits;                // 1 valid bit
+    int physPageBits;             // Bits required for the physical page number
+    int pteBits;                 // Total PTE size 
+    int ptByte;                  // Total RAM  for  page table
+} MemoryStats;
 
-    //# of pages  for  OS
-    int numOSPages = (numPages * os) / 100;
-
-    // # of virtual pages per process
-    int pages = VIRTUAL_ADDRESS_SPACE_BYTES / PAGE_SIZE;
-
-    int pteSizeBits = 19;
-    // total RAM needed 
-    int pageTable = (pages * pteSizeBits / 8) * files;
-
-    printf("----- Physical Memory & Page Table Calculations -----\n");
-    printf("Number of Physical Pages: %d\n", numPages);
-    printf("Number of Pages for System: %d\n", numOSPages);
-    printf("Size of Page Table Entry: %d bits\n", pteSizeBits);
-    printf("Total RAM Required for Page Table(s): %d bytes\n", pageTable);
+MemoryStats calculateMemoryStats(int physmem, int os, int files) {
+    MemoryStats stats;
+    
+    // Convert physmem to bytes
+    stats.physBytes = physmem * 1024 * 1024;
+    
+    // # of physical pages
+    stats.numPages = stats.physBytes / PAGE_SIZE;
+    
+    // # of OS pages
+    stats.numOSPages = (stats.numPages * os) / 100;
+    
+    // # of virtual pages 
+    int vPage = VIRTUAL_ADDRESS_SPACE_BYTES / PAGE_SIZE;
+    
+    // # of bits for  physical page number
+    int physPageBits = (int)ceil(log2((double)stats.numPages));
+    
+    // page table entry in bits
+    int pteSizeBits = 1 + physPageBits;
+    
+    // total RAM  for page table
+    int pageTableBytes = (vPage * pteSizeBits / 8) * files;
+    
+    // Fill in the struct.
+    stats.validBits = 1;
+    stats.physPageBits = physPageBits;
+    stats.pteBits = pteSizeBits;
+    stats.ptByte = pageTableBytes;
+    
+    return stats;
 }
 
-int main(int argc, char *argv[]) {
-    if (argc != 4) {
-        printf("Usage: %s <physical_memory_MB> <os_percentage> <num_trace_files>\n", argv[0]);
-        return 1;
-    }
-
-    int physmem = atoi(argv[1]);
-    int os = atoi(argv[2]);
-    int files = atoi(argv[3]);
-
-    calculateMemoryStats(physmem, os, files);
-
-    return 0;
-}
