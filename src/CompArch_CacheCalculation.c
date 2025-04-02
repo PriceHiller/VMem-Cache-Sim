a82e92b680e7931eb70e9b071d03b511e55cdcca
renaemanalastas04@gmail.com <renaemanalastas04@gmail.com>
2025-04-02 16:13:11 -0500

    refactor: changed return type of calculateCacheValues to struct CacheValues

--- src/CompArch_CacheCalculation.c
+++ src/CompArch_CacheCalculation.c
@@ -4,28 +4,34 @@
 
 // cache calculation
 
-int calculateBlocks(int cacheSize, int blockSize) {
-    return cacheSize / blockSize;
-}
-int calculateSets(int cacheSize, int blockSize, int associativity) {
-    return cacheSize / (blockSize * associativity);
-}
+typedef struct {
+    int cacheBits;
+    int sets;
+    int numBlocks;
+    int indexSize;
+    int offsetSize;
+    int tagSize;
+    int overheadSize;
+    double implementMemorySize;
+    double implementMemorySizeInKB;
+    double cost;
+} CacheValues;
+
+int calculateBlocks(int cacheSize, int blockSize) { return cacheSize / blockSize; }
+int calculateSets(int cacheSize, int blockSize, int associativity) { return cacheSize / (blockSize * associativity); }
 int calculateOffsetSize(int blockSize) { return log2f(blockSize); }
 int calculateIndexSize(int sets) { return log2f(sets); }
-double calculateImplementationMemorySize(int cacheSize, int overheadSize) {
-    return cacheSize + overheadSize;
-}
-
-int calculateTagSize(int physMem, int indexSize, int offsetSize) {
-    return log2f(physMem) - indexSize - offsetSize;
-}
+double calculateImplementationMemorySize(int cacheSize, int overheadSize) { return cacheSize + overheadSize; }
+int calculateTagSize(int physMem, int indexSize, int offsetSize) { return log2f(physMem) - indexSize - offsetSize; }
 
 int calculateOverheadSize(int numBlocks, int tagSize) {
     int overhead = numBlocks * (1 + tagSize);
     return overhead / 8;
 }
 
-void calculateCacheValues(int cacheSize, int blockSize, int associativity, int physMem) {
+CacheValues calculateCacheValues(int cacheSize, int blockSize, int associativity, int physMem) {
+    CacheValues cv;
+
     int cacheBits = log2f(cacheSize);
     int sets = calculateSets(cacheSize, blockSize, associativity);
     int numBlocks = calculateBlocks(cacheSize, blockSize);
@@ -35,33 +41,20 @@ void calculateCacheValues(int cacheSize, int blockSize, int associativity, int p
     int overheadSize = calculateOverheadSize(numBlocks, tagSize);
     int implementMemorySize =
         calculateImplementationMemorySize(cacheSize, overheadSize);
-    double implementMemorySizeKB = (double)(implementMemorySize / pow(2, 10));
-    double cost = (double)(0.12 * implementMemorySizeKB);
+    double implementMemorySizeInKB = (double)(implementMemorySize / pow(2, 10));
+    double cost = (double)(0.12 * implementMemorySizeInKB);
 
-    printf("Total # Blocks: \t\t%d\n", calculateBlocks(cacheSize, blockSize));
-    // printf("Cache Bits: \t\t\t%d bits\n", cacheBits);
-    printf("Tag Size: \t\t\t%d bits\n", tagSize);
-    printf("Index Size: \t\t\t%d bits\n", indexSize);
-    // printf("Offset Size: \t\t\t%d bits\n", offsetSize);
-    printf("Total # Rows: \t\t\t%d\n", sets);
-    printf("Overhead Size: \t\t\t%d bytes\n", overheadSize);
-    printf("Implementation Memory Size: \t%.2lf KB (%d bytes)\n",
-           implementMemorySizeKB, implementMemorySize);
-    printf("Cost: \t\t\t\t$%.2lf @ $0.12 per KB\n", cost);
-}
+    cv.cacheBits = cacheBits;
+    cv.sets = sets;
+    cv.numBlocks = numBlocks;
+    cv.indexSize = indexSize;
+    cv.offsetSize = offsetSize;
+    cv.tagSize = tagSize;
+    cv.overheadSize = overheadSize;
+    cv.implementMemorySize = implementMemorySize;
+    cv.implementMemorySizeInKB = implementMemorySizeInKB;
+    cv.cost = cost;
 
-int main(int argc, char *argv[]) {
-    if (argc < 3) {
-        printf("Not enough args.");
-        return 1;
-    }
 
-    int cacheSize = atoi(argv[1]) * pow(2, 10);
-    int blockSize = atoi(argv[2]);
-    int associativity = atoi(argv[3]);
-    int physMem = 128 * pow(2, 20); // 128 mb in example
-
-    calculateCacheValues(cacheSize, blockSize, associativity, physMem);
-
-    return 0;
+    return cv;    
 }
