0cf5b2ef67f4a9c4edab77a755ed0bc72bf019a1
Price Hiller <price@price-hiller.com>
2025-04-04 17:40:36 -0500

    refactor: use `#define` instead of static const

--- src/lib/types/types.h
+++ src/lib/types/types.h
@@ -1,9 +1,9 @@
 #ifndef LIBTYPES_H
 #define LIBTYPES_H
 
-static const unsigned short BYTES_KB = 1024;
-static const unsigned long BYTES_MB = (1024ULL * BYTES_KB);
-static const unsigned long long BYTES_GB = (1024ULL * BYTES_MB);
+#define BYTES_KB 1024
+#define BYTES_MB (1024ULL * BYTES_KB)
+#define BYTES_GB (1024ULL * BYTES_MB)
 
 typedef unsigned long long Byte;
 typedef unsigned long Kilobyte;
--- src/physmemcalc/physmemcalc.c
+++ src/physmemcalc/physmemcalc.c
@@ -1,8 +1,8 @@
 #include "../lib/types/physicalmemory.h"
 #include <math.h>
 
-static const Byte PAGE_SIZE_BYTES = 4 * BYTES_KB;
-static const Byte VIRTUAL_ADDRESS_SPACE_BYTES = 2ULL * BYTES_GB;
+#define PAGE_SIZE_BYTES (4 * BYTES_KB)
+#define VIRTUAL_ADDRESS_SPACE_BYTES (2ULL * BYTES_GB)
 
 PhysicalMemory PhysicalMemory_calculate_stats(Byte physical_memory_bytes,
                                               int percent_memory_used,
