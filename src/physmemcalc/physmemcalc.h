437b8345e2378e9a8db3b619d41853d18d87ec29
Price Hiller <price@price-hiller.com>
2025-04-06 14:29:55 -0500

    fix: make physmemcalc header match def

--- src/physmemcalc/physmemcalc.h
+++ src/physmemcalc/physmemcalc.h
@@ -1,5 +1,5 @@
 #ifndef PHYSMEMCALC_H
 #define PHYSMEMCALC_H
 #include "../lib/types/physicalmemory.h"
-PhysicalMemory PhysicalMemory_calculate_stats(int physmem, int os, int files);
+PhysicalMemory PhysicalMemory_calculate_stats(Byte physmem, int os, int files);
 #endif
