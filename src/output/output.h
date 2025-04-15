5c1c9df9e653e1e31c51d4b06674ef2faf004326
Makayla Frank <157774965+makaylafrank@users.noreply.github.com>
2025-04-15 14:58:55 -0500

    Update output.h

--- src/output/output.h
+++ src/output/output.h
@@ -3,7 +3,9 @@
 #include "../lib/types/cache.h"
 #include "../lib/types/physicalmemory.h"
 #include "../lib/types/types.h"
+#include "../virtmemresults/virtMemResults.h"
 char *CacheParameters_to_string(CacheParameters *params);
 char *CacheValues_to_string(CacheValues *cache_vals);
 char *PhysicalMemory_to_string(PhysicalMemory *phys_mem);
+char *VirtualMemory_to_string(SimulationStats *sim);
 #endif
