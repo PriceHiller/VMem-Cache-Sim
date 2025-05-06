66822a8ec83c67595b628c2fd0ebc744b472247a
Makayla Frank <157774965+makaylafrank@users.noreply.github.com>
2025-05-06 17:17:27 -0500

    Update output.h

--- src/output/output.h
+++ src/output/output.h
@@ -4,8 +4,12 @@
 #include "../lib/types/physicalmemory.h"
 #include "../lib/types/types.h"
 #include "../virtmemresults/virtMemResults.h"
+#include "../performance/performance.h"
+#include "../integration/integration.h"
+#include "../cachesim/cachesim.h"
 char *CacheParameters_to_string(CacheParameters *params);
 char *CacheValues_to_string(CacheValues *cache_vals);
 char *PhysicalMemory_to_string(PhysicalMemory *phys_mem);
 char *VirtualMemory_to_string(SimulationStats *sim);
+char *CacheSimulation_to_string(CacheAccess *access, PerfStats *perf, Cache *cache, CacheParameters *params)
 #endif
