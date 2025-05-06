e590f0c358a7a76cc5f84187084d46e9179a65d6
renaemanalastas04@gmail.com <renaemanalastas04@gmail.com>
2025-05-05 23:45:29 -0500

    feat: implemented cache access integration

---
+++ src/integration/integration.c
@@ -0,0 +1,34 @@
+#include "integration.h"
+
+CacheAccess access;
+
+unsigned int rowsAccessed(Address address, Byte addrSize, Byte blockSize){
+    return ((address + addrSize - 1) / blockSize) - (address / blockSize) + 1;
+}
+
+void sendToSim(Address address, Byte addrSize, Byte blockSize, AccessType type){
+    unsigned int rows = rowsAccessed(address, addrSize, blockSize);
+    access.totalAddresses++;
+    access.totalRowsAccessed += rows;
+    
+    if (type == EIP){
+        access.eipBytes += addrSize;
+        //increment total instructions by 1, e.g. totalInstructions++;
+        //increment total cycles by 2, e.g. totalCycles += 2;
+    } else if (type == SRC){
+        access.srcBytes += addrSize;
+        //increment total instructions by 1, e.g. totalInstructions++;
+        //increment total cycles by 1, e.g. totalCycles++;
+    } else if (type == DST){
+        access.dstBytes += addrSize;
+        //increment total instructions by 1, e.g. totalInstructions++;
+        //increment total cycles by 1, e.g. totalCycles++;
+    }
+    
+    unsigned int i;
+    for (i = 0; i < rows; i++){
+        unsigned int rowAddress = (address & ~(blockSize - 1)) + i * blockSize;
+        access.totalCacheAccesses++;
+    }
+    
+}
---
+++ src/integration/integration.h
@@ -0,0 +1,40 @@
+#ifndef INTEGRATION_H
+#define INTEGRATION_H
+#include "../lib/types/types.h"
+#include "../src/instructions/address.h"`
+
+/* cache.h, remove later */
+//#define BLOCK_SIZE 16
+//#define CACHE_SETS 8192
+//#define ASSOCIATIVITY 4
+//#define CACHE_SIZE (CACHE_SETS * ASSOCIATIVITY * BLOCK_SIZE)
+//#define COST_PER_KB 0.12
+/* end */
+
+/* types.h, remove later */
+//#define BYTES_KB 1024
+//#define BYTES_MB (1024ULL * BYTES_KB)
+//#define BYTES_GB (1024ULL * BYTES_MB)
+
+//typedef unsigned long long Byte;
+/* end */
+
+//typedef unsigned int Address; //address.h, remove later
+
+typedef enum { EIP, SRC, DST } AccessType;
+
+typedef struct {
+    unsigned int totalCacheAccesses;
+    unsigned int totalRowsAccessed;
+    Byte eipBytes; //instruction
+    Byte srcBytes;
+    Byte dstBytes;
+    unsigned int totalAddresses;
+} CacheAccess;
+
+extern CacheAccess access;
+
+unsigned int rowsAccessed(Address address, Byte addrSize, Byte blockSize);
+void sendToSim(Address address, Byte addrSize, Byte blockSize, AccessType type);
+
+#endif
